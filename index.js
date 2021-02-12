69f5ca5eaeb3d23e293e84ca85cd113101b56eca
Manoj Singh <manoj.singh@algoworks.com>
2021-02-12 22:03:51 +0530

    resolved issues

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 const Discord = require('discord.js');
-const {ErelaClient,Utils} = require('erela.js');
-const { prefix,token } = require('./config.json');
+const { ErelaClient, Utils } = require('erela.js');
+const { prefix, token } = require('./config.json');
 
 const client = new Discord.Client();
 
-//console.log(nodes);
+console.log(nodes);
 const nodes = [
     {
         host: "localhost",
@@ -19,60 +19,60 @@ client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	client.commands.set(command.name, command);
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
 }
 
 client.once('ready', () => {
     console.log('Ready!');
-    
-    client.music = new ErelaClient(client,nodes);
 
-    client.music.on("nodeConnect",()=>console.log("created a new node"));
+    client.music = new ErelaClient(client, nodes);
+
+    client.music.on("nodeConnect", () => console.log("created a new node"));
     console.log("1");
-    client.music.on("nodeError",console.log);
+    client.music.on("nodeError", console.log);
     console.log("2");
-    client.music.on("trackStart",({textChannel},{title,duration})=> textChannel.send(`Now playing:**${title}** \` ${Utils.formatTime(duration,true)}\` `));
+    client.music.on("trackStart", ({ textChannel }, { title, duration }) => textChannel.send(`Now playing:**${title}** \` ${Utils.formatTime(duration, true)}\` `));
     console.log("3");
-    client.music.on("queueEnd",player => {
+    client.music.on("queueEnd", player => {
         player.textChannel.send("Queue has ended")
         return client.music.players.destroy(player.guild.id);
     });
     console.log("4");
-    
+
     client.levels = new Map()
-    .set("none",0.0)
-    .set("low",0.10)
-    .set("medium",0.15)
-    .set("high",0.25);
+        .set("none", 0.0)
+        .set("low", 0.10)
+        .set("medium", 0.15)
+        .set("high", 0.25);
 
     process.on('uncaughtException', function (err) {
         console.log(err);
-    }); 
+    });
 
 });
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-	const args = message.content.slice(prefix.length).split(/ +/);
-	const commandName = args.shift().toLowerCase();
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const commandName = args.shift().toLowerCase();
 
-     //console.log(commandName);
+    //console.log(commandName);
 
     if (!client.commands.has(commandName)) return;
-    
+
     const command = client.commands.get(commandName);
 
-    
-	try {
-       // console.log(command);
+
+    try {
+        // console.log(command);
         // client.commands.get(command).execute(message, args);
-        command.execute(message,args);
-	} catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
 });
 
 
@@ -143,12 +143,12 @@ client.login(token);
 //         if(message.content.startsWith(`${prefix}nikal`)){
 //             client.commands.get('kick').execute(message);
 //         }
-    
+
 //         if(message.content.startsWith(`${prefix}hello`)){
 //       client.commands.get('assignRole').execute(message);
 //         }
 
-        
+
 
 //    // }
 // })
