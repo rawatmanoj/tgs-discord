4dcec3f17cea594cab7ca4d4a466beec2560188a
Manoj Singh <manoj.singh@algoworks.com>
2021-02-14 20:58:54 +0530

    resolved issues

--- commands.js
+++ commands.js
@@ -5,12 +5,12 @@ const commands = { reply };
 
 
 module.exports = async function (msg) {
-    if (msg.channel.id == "715786219770085396") {
-        let tokens = msg.content.split(" ");
-        let command = tokens.shift();
-        if (command.charAt(0) === "!") {
-            command = command.substring(1);
-            commands[command](msg, tokens);
-        }
+
+    let tokens = msg.content.split(" ");
+    let command = tokens.shift();
+    if (command.charAt(0) === "!") {
+        command = command.substring(1);
+        commands[command](msg, tokens);
     }
+
 };
