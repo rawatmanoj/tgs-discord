660c99297078db8b4273ad985f6d23de3c8dbda3
Manoj Singh <manoj.singh@algoworks.com>
2021-02-14 14:07:31 +0530

    resolved issues

--- commands/cooldown.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'ping',
-	cooldown: 5,
-	execute(message) {
-		message.channel.send('Pong.');
-	},
-};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const Discord = require('discord.js');
 const { ErelaClient, Utils } = require('erela.js');
+require('dotenv').config();
 const prefix = "_";
 //const {  token } = require('./config.json');
 
@@ -15,66 +16,12 @@ const nodes = [
     }
 ]
 
-client.commands = new Discord.Collection();
-
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.name, command);
-}
 
 client.once('ready', () => {
     console.log('Ready!');
-
-    // client.music = new ErelaClient(client, nodes);
-
-    // client.music.on("nodeConnect", () => console.log("created a new node"));
-    // console.log("1");
-    // client.music.on("nodeError", console.log);
-    // console.log("2");
-    // client.music.on("trackStart", ({ textChannel }, { title, duration }) => textChannel.send(`Now playing:**${title}** \` ${Utils.formatTime(duration, true)}\` `));
-    // console.log("3");
-    // client.music.on("queueEnd", player => {
-    //     player.textChannel.send("Queue has ended")
-    //     return client.music.players.destroy(player.guild.id);
-    // });
-    // console.log("4");
-
-    // client.levels = new Map()
-    //     .set("none", 0.0)
-    //     .set("low", 0.10)
-    //     .set("medium", 0.15)
-    //     .set("high", 0.25);
-
-    // process.on('uncaughtException', function (err) {
-    //     console.log(err);
-    // });
-
 });
 
-client.on('message', message => {
-    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).split(/ +/);
-    const commandName = args.shift().toLowerCase();
-
-    //console.log(commandName);
-
-    if (!client.commands.has(commandName)) return;
-
-    const command = client.commands.get(commandName);
-
-
-    try {
-        // console.log(command);
-        // client.commands.get(command).execute(message, args);
-        command.execute(message, args);
-    } catch (error) {
-        console.error(error);
-        message.reply('there was an error trying to execute that command!');
-    }
-});
 
 
 //music
